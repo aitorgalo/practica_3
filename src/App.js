c6137079b94d09f174cd08567fb356d7a4dc869e
Aitor Garzón <agarzon@ribesalat.com>
2021-06-07 14:01:05 +0200

    Searchable

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, useLocation } from "react-router-dom";
 import axios from 'axios';
 
  export default function App() {
  const [data, setData] = useState({ results: [] });
  
+ 
  useEffect( () => {
  const fetchData = async () => {
- const result = await axios('https://pokeapi.co/api/v2/pokemon?limit=10');
+ const result = await axios('https://pokeapi.co/api/v2/pokemon?limit=25');
  setData(result.data);
  };
  
@@ -17,11 +18,41 @@ import axios from 'axios';
  
  
  return (
- <ul>
+<Router>
+
+
+<Route path="/" exact component={ () => ( 
+
+<ul>
  {  data.results.map(item => ( <li key={item.name}> <a href={item.url}>{item.name}</a> </li> ))   }
  </ul>
- );
- 
- 
- 
+
+
+ ) } />
+
+<Route path ="/pokemons" component ={Pokemons} />
+
+</Router>
+);
+   
  }
+ 
+ function Pokemons()
+{
+return (  <div>
+<h1>Pokemons</h1>
+<ul>
+<li key={123}>
+<Link to
+="/pokemons/guitar"
+>Guitar</Link
+> </li
+>
+<li key={345}>
+<Link to="/pokemons/violin">Violin</Link> </li></ul>
+
+<Route path="/pokemons/:pokemonName" component={Pokemon} /> </div>  );
+
+}
+
+const Pokemon = ({match}) => <div> This is a nice {match.params.pokemonName} </div>;
