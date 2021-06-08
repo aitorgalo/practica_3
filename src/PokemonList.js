4ff0a1ffdeb254904420ba714c53977871cbcdb1
Aitor Garzón <agarzon@ribesalat.com>
2021-06-08 11:03:19 +0200

    New Image

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -5,11 +5,10 @@ import axios from 'axios';
 
 const PokemonList = () => {
 
-
     const [pokemonArray, setPokemons] = useState({ results: [] });
     useEffect(() => {
         const fetchPokemonList = async () => {
-            const result = await axios('https://pokeapi.co/api/v2/pokemon?limit=150');
+            const result = await axios('https://pokeapi.co/api/v2/pokemon?limit=151');
             setPokemons(result.data);
         };
 
@@ -20,7 +19,7 @@ const PokemonList = () => {
     return (
 
         <ul>
-            {pokemonArray.results.map(item => (<li key={item.name}>  <NavLink to={"/pokemon/" + item.name}>{item.name.toUpperCase()}</NavLink>              </li>))}
+            {pokemonArray.results.map(item => (<li key={item.name}>  <NavLink activeStyle={{ backgroundColor: 'green' }} to={"/pokemon/" + item.name}>{item.name.toUpperCase()}</NavLink>              </li>))}
         </ul>
 
     );
